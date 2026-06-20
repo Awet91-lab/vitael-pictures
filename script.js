875d1ca6f084a9c9f65cecfec345810ebfb6d818
Awet Tsegai <awetsegai1991@Gmail.com>
2026-06-20 07:34:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+document.getElementById("contactForm")
+.addEventListener("submit", function (e) {
+
+e.preventDefault();
+
+const name = document.getElementById("name").value;
+const email = document.getElementById("email").value;
+const message = document.getElementById("message").value;
+
+const phone = "256756700259";
+
+const text = `Hello Vitael Pictures
+
+Name: ${name}
+Email: ${email}
+Message: ${message}`;
+
+const url = `https://wa.me/${phone}?text=${encodeURIComponent(text)}`;
+
+window.open(url, "_blank");
+
+// reset form
+document.getElementById("contactForm").reset();
+
+// show popup
+showToast("Request sent successfully ✔");
+
+});
+
+
+// Slider
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+slides[index].classList.remove("active");
+index = (index + 1) % slides.length;
+slides[index].classList.add("active");
+}, 4000);
+// COLLAPSIBLE FOOTER
+function toggleFooter(el){
+el.parentElement.classList.toggle("active");
+}
+
+function showToast(message){
+const toast = document.getElementById("toast");
+toast.innerText = message;
+toast.classList.add("show");
+
+setTimeout(() => {
+toast.classList.remove("show");
+}, 2500);
+}
